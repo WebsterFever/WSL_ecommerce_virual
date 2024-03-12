4dd7b316f65eb18ec806d94cd2965af11a618c13
WebsterFever <webstersevenlanguages@gmail.com>
2024-03-12 13:10:37 -0400

    first commit

---
+++ src/main/java/ecommerce_virtual_WSL/wsl/WslApplication.java
@@ -0,0 +1,13 @@
+package ecommerce_virtual_WSL.wsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecommerce_virtual_WSL/wsl/WslApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce_virtual_WSL.wsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
